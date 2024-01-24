5efc1ebfa9b339b3fe6cfd8b152c3e2e01863b8f
ngdatdev <nvdatdz8b@gmail.com>
2024-01-24 17:12:20 +0700

    fix

---
+++ LAB211/week_3_manage-students/src/controller_view/App.java
@@ -0,0 +1,13 @@
+
+package controller_view;
+
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new StudentController().run();
+    }
+    
+}
